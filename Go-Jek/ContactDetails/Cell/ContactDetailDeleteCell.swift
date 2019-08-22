43be504b7468fad777047812fe188a1afcceae89
satendra singh <satendra.singh@yatra.com>
2019-08-22 10:24:14 +0530

    Storyboard and assets changes

---
+++ Go-Jek/ContactDetails/Cell/ContactDetailDeleteCell.swift
@@ -0,0 +1,26 @@
+//
+//  ContactDetailDeleteCell.swift
+//  Go-Jek
+//
+//  Created by Satendra Singh on 21/08/19.
+//  Copyright © 2019 Satendra Singh. All rights reserved.
+//
+
+
+ import UIKit
+
+class ContactDetailDeleteCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
+
